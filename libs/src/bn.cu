8305ff39699e4b0544b744bd2ba7bc44df79b151
wzn0828 <18001279185@163.com>
2019-04-23 06:04:24 -0400

    Modify the backward of weight

--- libs/src/bn.cu
+++ libs/src/bn.cu
@@ -216,10 +216,7 @@ __global__ void backward_kernel(const float *dz, const float *z, const float *va
 
     if (dweight != 0) {
       if (threadIdx.x == 0) {
-        if (weight[plane] > 0)
-          dweight[plane] += _eydz * norm;
-        else if (weight[plane] < 0)
-          dweight[plane] -= _eydz * norm;
+        dweight[plane] += _eydz * norm;
       }
     }
 
