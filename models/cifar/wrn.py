10db5ac78bae780e6c72a31e90fa63bec7df0492
wzn0828 <18001279185@163.com>
2019-10-05 03:35:59 -0400

    In cifar's wrn, rename the fc to classify, and reture the outputs and feature

--- models/cifar/wrn.py
+++ models/cifar/wrn.py
@@ -63,7 +63,7 @@ class WideResNet(nn.Module):
         # global average pooling and classifier
         self.bn1 = custom.BN_Class(nChannels[3])
         self.relu = nn.ReLU(inplace=True)
-        self.fc = custom.Linear_Class(nChannels[3], num_classes)
+        self.classifier = custom.Linear_Class(nChannels[3], num_classes)
         self.nChannels = nChannels[3]
 
         for m in self.modules():
@@ -84,8 +84,8 @@ class WideResNet(nn.Module):
         out = self.block3(out)
         out = self.relu(self.bn1(out))
         out = F.avg_pool2d(out, 8)
-        out = out.view(-1, self.nChannels)
-        return self.fc(out)
+        feature = out.view(-1, self.nChannels)
+        return self.classifier(feature), feature
 
 def wrn(**kwargs):
     """
