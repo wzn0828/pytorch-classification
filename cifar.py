da15292c930bb7f243c20a762df3ea622aa1fc31
wzn0828 <18001279185@163.com>
2020-03-20 09:31:13 +0800

    when compute accuracy, use cosine or theta loss, replace the outputs with cosine

--- cifar.py
+++ cifar.py
@@ -328,12 +328,12 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
         outputs, features = model(inputs, targets)
         outputs, cosine = outputs
         if args.loss_type == 'softmax':
-            loss = criterion(outputs, targets)
+            pred = outputs
         else:
-            loss = criterion(cosine, targets)
-
+            pred = cosine
+        loss = criterion(pred, targets)
         # measure accuracy and record loss
-        prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
+        prec1, prec5 = accuracy(pred.data, targets.data, topk=(1, 5))
         losses.update(loss.item(), inputs.size(0))
         top1.update(prec1.item(), inputs.size(0))
         top5.update(prec5.item(), inputs.size(0))
