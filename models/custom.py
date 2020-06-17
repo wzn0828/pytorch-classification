080a8829b7546f1bdcc15b5e85ff294020a76958
wzn0828 <18001279185@163.com>
2020-06-17 19:57:24 +0800

    Add a new margin type : random-uniform

--- models/custom.py
+++ models/custom.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+import random
 
 
 Linear_Class = nn.Linear
@@ -884,6 +885,32 @@ class ArcClassify(nn.Linear):
         if not self.training or (self.margin_theta == 0 and self.margin_cosine == 0):
             output = cosine           # B x class_num
         elif self.margin_type == 'label':
+            if self.margin_theta != 0:
+                nB = len(embbedings)    # Batchsize
+
+                # pick the labeled cos theta
+                idx_ = torch.arange(0, nB, dtype=torch.long)
+                labeled_cos = cosine[idx_, label]            # B
+                # compute labeled theta
+                labeled_theta = torch.acos(labeled_cos)         # B
+                #compute the added margin
+                m = self.get_m(labeled_theta)
+                if self.only_margin_right:
+                    m *= (cosine.argmax(dim=1)==label).to(torch.float)
+                # add margin
+                labeled_theta += m                         # B
+                labeled_theta.clamp_(max=math.pi)
+                # compute the diff and expand it
+                labeled_diff = torch.cos(labeled_theta) - labeled_cos  # B
+                diff_expand = labeled_diff.unsqueeze(dim=1) * F.one_hot(label, num_classes=self.out_features).to(dtype=torch.float)  # B x class_num
+                if self.detach_diff:
+                    diff_expand.detach_()
+                # add diff and multiply the scale
+                cosine = cosine + diff_expand                     # B x class_num
+            output = cosine - self.margin_cosine * F.one_hot(label, num_classes=self.out_features).to(dtype=torch.float)
+        elif self.margin_type == 'random-uniform':
+            index = random.randint(0, self.out_features-1)
+            label = index * torch.ones_like(label)
             if self.margin_theta != 0:
                 nB = len(embbedings)    # Batchsize
 
